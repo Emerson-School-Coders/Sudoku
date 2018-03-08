be78504a398a6d57fd4d0f2d2f667e22eee222bc
JackMacWindows <jackbruienne@yahoo.com>
2018-03-08 13:48:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,31 @@
 int main(int argc, const char * argv[]) {
   Sudoku::Board board(argv[1]);
   int retval = 2;
+  std::cout << "Trying old algorithm\n";
   while (true) {
     try {
-      board.calculateGuesses();
+      board.calculateGuesses_old();
     } catch (std::string err) {
       std::cout << err << std::endl;
       retval = 1;
       break;
     }
-    if (board.confirmGuesses()) {
+    if (board.confirmGuesses_old()) {
+      std::cout << "Puzzle solved!\n";
+      retval = 0;
+      break;
+    }
+  }
+  std::cout << "Trying new algorithm\n";
+  while (true) {
+    try {
+      board.calculateGuesses_new();
+    } catch (std::string err) {
+      std::cout << err << std::endl;
+      retval = 1;
+      break;
+    }
+    if (board.confirmGuesses_new()) {
       std::cout << "Puzzle solved!\n";
       retval = 0;
       break;
